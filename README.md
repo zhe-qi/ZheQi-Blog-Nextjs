55b856cdb3e183e67be2d73ce345901ecd369b03
zhe-qi <zheqi0910@gmail.com>
2023-06-01 23:57:07 +0800

    feat: 更新项目

--- README.md
+++ README.md
@@ -1,28 +1,11 @@
-# Create T3 App
+# ZheQi-Blog
 
-This is a [T3 Stack](https://create.t3.gg/) project bootstrapped with `create-t3-app`.
+使用了create t3 app 搭建个人博客
 
-## What's next? How do I make an app with this?
+1. 拉取项目后新建.env文件把.env.example的内容复制进去
+2. 修改prisma中的mysql为sqlite
+3. 执行pnpm install
+4. 执行npx prisma db push
+5. 执行pnpm dev
 
-We try to keep this project as simple as possible, so you can start with just the scaffolding we set up for you, and add additional things later when they become necessary.
-
-If you are not familiar with the different technologies used in this project, please refer to the respective docs. If you still are in the wind, please join our [Discord](https://t3.gg/discord) and ask for help.
-
-- [Next.js](https://nextjs.org)
-- [NextAuth.js](https://next-auth.js.org)
-- [Prisma](https://prisma.io)
-- [Tailwind CSS](https://tailwindcss.com)
-- [tRPC](https://trpc.io)
-
-## Learn More
-
-To learn more about the [T3 Stack](https://create.t3.gg/), take a look at the following resources:
-
-- [Documentation](https://create.t3.gg/)
-- [Learn the T3 Stack](https://create.t3.gg/en/faq#what-learning-resources-are-currently-available) — Check out these awesome tutorials
-
-You can check out the [create-t3-app GitHub repository](https://github.com/t3-oss/create-t3-app) — your feedback and contributions are welcome!
-
-## How do I deploy this?
-
-Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/vercel), [Netlify](https://create.t3.gg/en/deployment/netlify) and [Docker](https://create.t3.gg/en/deployment/docker) for more information.
+执行npx prisma studio查看数据库
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,54 @@
 import { type NextPage } from 'next'
 import { api } from '~/utils/api'
 import { useRouter } from 'next/router'
+import dayjs from 'dayjs'
 
 const Home: NextPage = () => {
   const { locale } = useRouter()
 
   const hello = api.example.hello.useQuery({ locale: locale! })
 
-  const text = hello.data?.greeting
+  const getAll = api.example.getAll.useQuery()
 
+  const text = hello.data?.greeting
+  const data = getAll.data
+  console.log(data)
   return (
     <>
-      <div className="mt-20 flex h-[2000px] flex-col dark:text-red-200">
+      <div className="mx-auto mt-20 flex h-[2000px] max-w-4xl flex-col border-x dark:text-red-200">
         <div className="mt-20 text-center text-2xl">
           {locale === 'zh' ? '你好' : 'Hello'}
         </div>
-        <div className="mt-2 text-center">服务端数据：{text}</div>
+        <p className="mt-10 text-center">
+          {locale === 'zh' ? `服务端数据：${text!}` : `server data: ${text!}`}
+        </p>
+
+        <div className="mx-auto mt-10 text-center">
+          <p className="mb-2">
+            {locale === 'zh' ? `数据库数据：` : `sql data: `}
+          </p>
+          {data?.map((item) => {
+            return (
+              <section className="prose" key={item.id}>
+                <p>id：{item.id}</p>
+                <p>
+                  {locale === 'zh' ? `创建时间：` : `createdAt: `}
+                  {dayjs(item.createdAt).format('YYYY-MM-DD HH:mm:ss')}
+                </p>
+                <p>
+                  {locale === 'zh' ? `更新时间：` : `updatedAt: `}
+                  {dayjs(item.updatedAt).format('YYYY-MM-DD HH:mm:ss')}
+                </p>
+              </section>
+            )
+          })}
+        </div>
+        <p className="mt-10 text-center">
+          {' '}
+          {locale === 'zh'
+            ? `有时候可能会有水合错误，他*的`
+            : `have time have water * error, f**k he`}
+        </p>
       </div>
     </>
   )
